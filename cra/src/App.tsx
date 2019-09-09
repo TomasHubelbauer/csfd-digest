a025364692b9f0c10bad61e2181cd3a813841098
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-09-09 15:56:26 +0200

    Implement a CRA app with tonight's movies

--- cra/src/App.tsx
+++ cra/src/App.tsx
@@ -1,26 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component, ChangeEventHandler } from 'react';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+type Movie = {
+  id: string;
+  url: string;
+  name: string;
+  year: number;
+  screenings: {
+    [cinema: string]: Date[];
+  };
+  content: string;
+  imdbUrl: string;
+  posterUrl: string;
+  trailerUrl: string;
+};
+
+type Data = {
+  dateAndTime: Date;
+  cinemas: string[];
+  movies: Movie[];
+};
+
+type AppProps = {};
+
+type AppState = {
+  data:
+  | { type: 'loading' }
+  | { type: 'success', data: Data }
+  | { type: 'error', error: Error }
+  ;
+  selectedCinemas: string[];
+};
+
+export default class App extends Component<AppProps, AppState> {
+  public readonly state: AppState = {
+    data: { type: 'loading' },
+    selectedCinemas: localStorage.getItem('selected-cinemas') ? JSON.parse(localStorage.getItem('selected-cinemas')!) : [],
+  };
+
+  private readonly handleCinemaSelectChange: ChangeEventHandler<HTMLSelectElement> = event => {
+    const selectedCinemas = Array.from(event.currentTarget.options).filter(option => option.selected).map(option => option.value);
+    localStorage.setItem('selected-cinemas', JSON.stringify(selectedCinemas));
+    this.setState({ selectedCinemas });
+  };
+
+  public render() {
+    if (this.state.data.type === 'loading') {
+      return 'Loading…';
+    }
+
+    if (this.state.data.type === 'error') {
+      return 'Error!';
+    }
+
+    const now = new Date();
+
+    const midnightTonight = new Date();
+    midnightTonight.setHours(23, 59, 59, 999);
+
+    const screeningsTonight = this.state.data.data.movies
+      .map(movie => {
+        const screenings: any[] = [];
+
+        for (const cinema of this.state.selectedCinemas) {
+          if (!movie.screenings[cinema]) {
+            continue;
+          }
+
+          for (const screening of movie.screenings[cinema].filter(dateAndTime => dateAndTime > now && dateAndTime < midnightTonight)) {
+            screenings.push({ cinema, screening });
+          }
+        }
+
+        if (screenings.length === 0) {
+          return null;
+        }
+
+        return { movie, screenings };
+      })
+      .filter(screening => screening !== null);
+
+    return (
+      <div>
+        <h1>Prague Cinema Tonight</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Built by
+          <img alt="" src="https://hubelbauer.net/favicon.ico" />
+          <a href="https://hubelbauer.net" target="_blank" rel="noopener noreferrer">Tomas Hubelbauer</a>
+          .
+          <a href="https://github.com/TomasHubelbauer/puppeteer-csfd-scraper" target="_blank" rel="noopener noreferrer">GitHub</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <p>{this.state.selectedCinemas.length} cinema{this.state.selectedCinemas.length > 1 ? 's' : ''} selected</p>
+        <select multiple onChange={this.handleCinemaSelectChange} value={this.state.selectedCinemas}>
+          {this.state.data.data.cinemas.map(cinema => <option key={cinema}>{cinema}</option>)}
+        </select>
+        {screeningsTonight.map(screening => (
+          <div key={screening!.movie.id}>
+            <h2>{screening!.movie.name}</h2>
+            <img alt={`${screening!.movie.name} poster`} src={screening!.movie.posterUrl} />
+            <p>{screening!.movie.content}</p>
+            <ul>
+              {screening!.screenings.map((screen, index) => <li key={index}>{screen.cinema} {screen.screening.toLocaleTimeString()}</li>)}
+            </ul>
+          </div>
+        ))}
+      </div>
+    );
+  }
 
-export default App;
+  public async componentDidMount() {
+    const response = await fetch('data.json');
+    const data: Data = await response.json();
+    data.dateAndTime = new Date(data.dateAndTime);
+    for (const movie of data.movies) {
+      for (const cinema of data.cinemas) {
+        if (movie.screenings[cinema]) {
+          movie.screenings[cinema] = movie.screenings[cinema].map(dateAndTime => new Date(dateAndTime));
+        }
+      }
+    }
+
+    this.setState({ data: { type: 'success', data } });
+  }
+};
