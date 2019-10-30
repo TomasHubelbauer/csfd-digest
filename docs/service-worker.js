4c47784d3f8eeea5cb837004aa59659e21cfb016
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-10-30 00:21:21 +0000

    Generate GitHub Pages

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/puppeteer-csfd-scraper/docs/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
